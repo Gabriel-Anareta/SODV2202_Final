095dae4c0609c05112a75f3baca052f9cb89dd29
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-11-01 13:35:20 -0600

    Created basic server listener

--- ChessServer/Program.cs
+++ ChessServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,16 @@ namespace ChessServer
 {
     internal class Program
     {
+        private const int PORT = 5000;
+        private static TcpListener _listener;
+        
         static void Main(string[] args)
         {
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), PORT);
+            _listener.Start(); // implement blacklog ?
+
+            var client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client has connected!");
         }
     }
 }
