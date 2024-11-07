0a667552c62e2d31399ce588671b948d8bf21656
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-11-06 21:53:00 -0700

    Created Direction class

--- ChessLogic/Positions/Position.cs
+++ ChessLogic/Positions/Position.cs
@@ -1,15 +1,12 @@
 ﻿namespace ChessModel
 {
-    public class Position // represents a position given by a file and a rank
+    /// <summary>
+    /// represents a position given by a file and a rank
+    /// </summary>
+    public class Position(int file, int rank)
     {
-        public int File { get; set; }   // columns
-        public int Rank { get; set; }   // rows
-
-        public Position(int file, int rank)
-        {
-            File = file;
-            Rank = rank;
-        }
+        public int File { get; set; } = file;
+        public int Rank { get; set; } = rank;
 
         public PlayerColor SquareColor()
         {
@@ -39,6 +36,17 @@
         public static bool operator !=(Position? left, Position? right)
         {
             return !(left == right);
+        }
+
+        /// <summary>
+        /// overloaded + operator on position
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="dir"></param>
+        /// <returns>Returns a new position with the added direction value</returns>
+        public static Position operator +(Position pos, Direction dir)
+        {
+            return new Position(pos.File + dir.FileDelta, pos.Rank + dir.RankDelta);
         }
     }
 }
