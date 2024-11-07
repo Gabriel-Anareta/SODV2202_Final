d6c6b577c66d9b1e5e51bc9cd106c888f660cb0a
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-11-06 21:32:28 -0700

    Created PlayerManager class

---
+++ ChessLogic/Players/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessModel.Players
+{
+    public static class PlayerManager
+    {
+        public static PlayerColor Next(this PlayerColor color)
+        {
+            return color switch
+            {
+                PlayerColor.White => PlayerColor.Black,
+                PlayerColor.Black => PlayerColor.White,
+                PlayerColor.Red => PlayerColor.Green,
+                PlayerColor.Green => PlayerColor.Yellow,
+                PlayerColor.Yellow => PlayerColor.Blue,
+                PlayerColor.Blue => PlayerColor.Red,
+                _ => PlayerColor.None
+            };
+        }
+    }
+}
