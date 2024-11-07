b0d5beea7228676670eb724bee59e243f54bc770
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-11-06 23:06:02 -0700

    Created GameState class

--- ChessLogic/Boards/Board2Player.cs
+++ ChessLogic/Boards/Board2Player.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessModel
+﻿namespace ChessModel
 {
     public class Board2Player : Board
     {
---
+++ ChessLogic/Game/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessModel
+{
+    /// <summary>
+    /// Handles the game logic of the chess game
+    /// </summary>
+    public class GameState(Board board, PlayerColor player)
+    {
+        public Board GameBoard { get; set; } = board;
+        public PlayerColor CurrentPlayer { get; set; } = player;
+    }
+}
