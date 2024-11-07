f47f4cd21865c6b528c01e5dc60cee1b82d6fe9d
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-11-06 23:13:04 -0700

    Created Move class

---
+++ ChessLogic/Moves/Move.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessModel.Moves
+{
+    /// <summary>
+    /// Represents a move on a board
+    /// </summary>
+    public abstract class Move
+    {
+        public abstract MoveType Type { get; }
+        public abstract Position From { get; }
+        public abstract Position To { get; }
+
+        /// <summary>
+        /// Executes the current move on the given board
+        /// </summary>
+        /// <param name="board"></param>
+        public abstract void Execute(Board board);
+    }
+}
