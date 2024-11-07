2d025b787f0a24c7e8bd334898e57c8bb9a0e1c3
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-11-06 23:16:53 -0700

    Created NormalMove class

--- ChessLogic/Moves/Move.cs
+++ ChessLogic/Moves/Move.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChessModel.Moves
+namespace ChessModel
 {
     /// <summary>
     /// Represents a move on a board
--- ChessLogic/Moves/MoveType.cs
+++ ChessLogic/Moves/MoveType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChessModel.Moves
+namespace ChessModel
 {
     /// <summary>
     /// Represents the type of a move
