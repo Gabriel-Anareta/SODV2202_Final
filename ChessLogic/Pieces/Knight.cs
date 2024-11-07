980afc290ebc87476bb1be328addd9dc2a5345da
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-11-06 23:31:41 -0700

    Implemented GetMoves for Knight

--- ChessLogic/Pieces/Knight.cs
+++ ChessLogic/Pieces/Knight.cs
@@ -21,5 +21,28 @@ namespace ChessModel
             copy.HasMoved = this.HasMoved;
             return copy;
         }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+            => ValidJumps(from, board)
+                .Select(pos => new NormalMove(from, pos));
+
+        private IEnumerable<Position> ValidJumps(Position from, Board board)
+            => TheoreticalJumps(from)
+                .Where(pos =>
+                    board.IsValidPosition(pos)
+                    && (board.IsEmptyPosition(pos) || board[pos].Color != this.Color)
+                );
+
+        private IEnumerable<Position> TheoreticalJumps(Position from)
+        {
+            foreach (Direction vDir in new List<Direction> { Direction.North, Direction.South })
+            {
+                foreach (Direction hDir in new List<Direction> { Direction.West, Direction.East })
+                {
+                    yield return from + 2 * vDir + hDir;
+                    yield return from + 2 * hDir + vDir;
+                }
+            }
+        }
     }
 }
