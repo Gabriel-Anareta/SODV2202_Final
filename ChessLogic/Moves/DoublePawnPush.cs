f344a7235a7e6cf753ba5fb24bd7c321832b4590
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-11-07 23:40:31 -0700

    Created DoublePawnPush class

---
+++ ChessLogic/Moves/DoublePawnPush.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessModel.Moves
+{
+    public class DoublePawnPush : Move
+    {
+        public override MoveType Type => MoveType.DoublePawnPush;
+        public override Position From { get; }
+        public override Position To { get; }
+
+        private readonly Position _enPassantSquare;
+
+        public DoublePawnPush(Position from, Position to, PlayerColor color)
+        {
+            From = from;
+            To = to;
+            _enPassantSquare = color switch
+            {
+                PlayerColor.White => new Position(From.File, (From.Rank + To.Rank) / 2),
+                PlayerColor.Black => new Position(From.File, (From.Rank + To.Rank) / 2),
+                PlayerColor.Red => new Position(From.File, (From.Rank + To.Rank) / 2),
+                PlayerColor.Green => new Position((From.File + To.File) / 2, From.Rank),
+                PlayerColor.Yellow => new Position(From.File, (From.Rank + To.Rank) / 2),
+                _ => new Position((From.File + To.File) / 2, From.Rank)
+            };
+        }
+
+        public override void Execute(Board board)
+        {
+            PlayerColor color = board[From].Color;
+            board.SetEnPassantSquare(color, _enPassantSquare);
+
+            new NormalMove(From, To).Execute(board);
+        }
+    }
+}
